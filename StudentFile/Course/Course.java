d1bca517ea9f466453a3f85f7bd89a41a55fbf9e
Jaylen Smith <jaylensmith4521@gmail.com>
2023-03-07 15:41:22 -0500

    Final touches

---
+++ StudentFile/Course/Course.java
@@ -0,0 +1,20 @@
+package StudentFile.Course;
+
+public class Course {
+    private String title;
+    private String instructorName;
+    private String days;
+    private String duration;
+
+    public Course(String title, String instructorName, String days, String duration) {
+        this.title = title;
+        this.instructorName = instructorName;
+        this.days = days;
+        this.duration = duration;
+    }
+
+    public void showCourseInformation() {
+        System.out.printf("Course title: %s\nInstructor: %s\nDays: %s\nDuration: %s\n", this.title, this.instructorName, this.days, this.duration);
+        System.out.println("-----------------------------");
+    }
+}
--- StudentFile/Student.java
+++ StudentFile/Student.java
@@ -1,5 +1,7 @@
 package StudentFile;
 
+import StudentFile.Course.*;;
+
 public class Student {
     String fullname;
     int age;
@@ -10,5 +12,15 @@ public class Student {
         this.fullname = fullname;
         this.age = age;
         this.courses = courses;
+    }
+
+    public void printInformation() {
+        System.out.printf("Hello %s!", fullname);
+        System.out.printf("From what we see, you are %d years old", age);
+        System.out.printf("You are taking %d courses", courses.length);
+
+        for(Course course: this.courses) {
+            course.showCourseInformation();
+        }
     }
 }
