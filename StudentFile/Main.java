6910b7a1c2143d0776b1cdd138143de7b24833a7
Jaylen Smith <jaylensmith4521@gmail.com>
2023-03-07 15:40:29 -0500

    Finishing touches

--- StudentFile/Main.java
+++ StudentFile/Main.java
@@ -1,15 +1,21 @@
 package StudentFile;
 
 import java.util.Scanner;
+import StudentFile.Course.*;
 
 public class Main {
 
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
+
+        Student student;
+
         String name = inputName();
         int age = inputAge();
+        Course[] courses = inputCourses();
 
-        Course[] courses;
+        student = new Student(name, age, courses);
+        student.printInformation();
     }
 
     public static String inputName() {
@@ -38,6 +44,26 @@ public class Main {
     }
 
     public static Course[] inputCourses() {
-        
+        Course[] courses = {};
+
+        for(int i = 0; i < 3; i++) {
+            String title;
+            String instructorName;
+            String days;
+            String duration; 
+
+            System.out.println("What is the name of course #"+(i+1));
+            title = input.nextLine();
+            System.out.println("Who is the instructor of course?");
+            instructorName = input.nextLine();
+            System.out.println("What days does this course take place? Type in full format such as 'Monday'");
+            days = input.nextLine();
+            System.out.println("How long is this class?");
+            duration = input.nextLine();
+
+            courses[i] = new Course(title, instructorName, days, duration);
+        }
+
+        return courses;
     }
 }
