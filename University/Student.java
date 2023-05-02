c5da9a29c9b56b2a5bdc8e3e2e75c1b7c385c81b
Jaylen Smith <jaylensmith4521@gmail.com>
2023-05-02 16:49:11 -0400

    ID Checking

--- University/Student.java
+++ University/Student.java
@@ -1,6 +1,6 @@
 package University;
 
-import StringStream.main;
+import java.util.ArrayList;
 
 public class Student {
 
@@ -15,14 +15,17 @@ public class Student {
         this.configure();
     
         //Leave zero for now
-        this.studentID = 0;
+        Student.ID += 1;
     }
 
     public Student(Student[] students, int id) {
-        
-        if (this.alreadyHasExisitingID(students, id)) {
 
+        System.out.println(this.alreadyHasExisitingID(students, id));
+        if (this.alreadyHasExisitingID(students, id)) {
+            ID = setID(students, id);
         }
+
+        ID = id;
 
         this.configure();
     }
@@ -34,17 +37,41 @@ public class Student {
         return false;
     }
 
+    private int setID(Student[] students, int id) {
+        ArrayList<Student> studentList = new ArrayList<Student>();
+
+        if (!studentList.isEmpty()) {
+            return id;
+        }
+
+        //Adding students to the student list
+        for(Student student: students) {
+            studentList.add(student);
+        }
+
+        if (studentList.contains(studentList.get(id-1))) {
+            System.out.println("That id is taken already!");
+            return studentList.get(ID-1).ID + 1;
+        }
+
+        return id;
+    }
+
     private void configure() {
         this.name = "Unknown";
         this.age = 0;
         this.gpa = 0;
+        this.studentID = 0;
     }
 
     public void information() {
-        System.out.printf("Name: %s", name);
-        System.out.printf("Age: %d", age);
-        System.out.printf("GPA: %d", gpa);
-        System.out.printf("Student ID: %d", studentID);
+        System.out.println("\n----------");
+        System.out.printf("Name: %s\n", name);
+        System.out.printf("Age: %d\n", age);
+        System.out.printf("GPA: %f\n", gpa);
+        System.out.printf("Student ID: %d\n", studentID);
+        System.out.printf("Identifer: %d\n", ID);
+        System.out.println("----------\n");
     }
 
 
--- University/University.java
+++ University/University.java
@@ -1,7 +1,24 @@
 package University;
 
+import StringStream.main;
+
 public class University {
     
-
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.information();
+        Student student2 = new Student();
+        student2.information();
+        Student student3 = new Student();
+        student3.information();
+        Student student4 = new Student();
+        student4.information();
     
+        Student[] students = {student1, student2, student3, student4};
+    
+        Student student5 = new Student(students, 18);
+
+        student5.information();
+
+    }
 }
