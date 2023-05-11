71dfb01ce5e48f72a6e3e2354900126a811f24f3
Cameron W <wildcatjake21@gmail.com>
2023-05-11 17:29:12 -0400

    Add files via upload
    food ordering system

---
+++ FoodOrderingSystem/OrderSystem.java
@@ -0,0 +1,14 @@
+package FoodOrderingSystem;
+
+public class OrderSystem {
+
+    static final Food[] foodMenu = {
+        new Food("Pizza", 3.00),
+        new Food("Soda", 0.75),
+        new Food("Hamburger", 5.00),
+    };
+    public static void main(String[] args) {
+
+
+    }
+}
